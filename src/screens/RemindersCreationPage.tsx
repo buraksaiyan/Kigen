785f482bd91fed2140cb9c291031ff8a24fcb022
Burak Furkan Köken <burakthef1master@gmail.com>
2025-09-25 13:01:45 +0300

    alert something is deprecated so i updated it

--- src/screens/RemindersCreationPage.tsx
+++ src/screens/RemindersCreationPage.tsx
@@ -36,7 +36,6 @@ interface RemindersCreationPageProps {
 // Configure notifications
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: false,
     shouldShowBanner: true,
