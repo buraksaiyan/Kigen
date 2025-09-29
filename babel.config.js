3c158d7f513bbc6f4a0ff303a2b0092e1510469f
Burak Furkan Köken <burakthef1master@gmail.com>
2025-09-29 09:01:08 +0300

    error fix

--- babel.config.js
+++ babel.config.js
@@ -8,8 +8,9 @@ module.exports = function(api) {
         {
           globals: ['__scanFaces'],
         },
+        'react-native-worklets-plugin',
       ],
-      'react-native-reanimated/plugin',
+      ['react-native-reanimated/plugin', {}, 'react-native-reanimated-plugin'],
     ],
   };
 };
